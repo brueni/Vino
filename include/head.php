834ab28372614817a88832ebeaae115e1b46520f
Michael Brünisholz <info@michael-bruenisholz.ch>
2016-09-07 22:19:54 +0200

    switched from phpthumb to simpleimage

--- include/head.php
+++ include/head.php
@@ -21,5 +21,5 @@
   <link href="css/custom.css" rel="stylesheet">
 </head>
 <?php include('config.inc.php');
-require_once('include/phpthumb/ThumbLib.inc.php'); ?>
+include('include/simpleimage/simpleimage.php') ?>
   <body role="document" style="background-image: url(include/img/bg_wall.jpg)">
